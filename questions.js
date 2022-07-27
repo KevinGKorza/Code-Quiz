039dcd95888490a184a2ebbca5876f3719786aec
Kevin Korza <107637260+KevinGKorza@users.noreply.github.com>
2022-07-27 14:01:44 -0400

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,1 +1,60 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What is Homer Simpsons favorite food?",
+    answer: "Donuts",
+    options: [
+      "Ice Cream",
+      "Donuts",
+      "Pizza",
+      "Cheeseburgers"
+    ]
+  },
+
+  {
+    numb: 2,
+    question: "Who is Disney's Mascot?",
+    answer: "Mickey Mouse",
+    options: [
+      "Darth Vader",
+      "Pluto",
+      "Mickey Mouse",
+      "Donold Duck"
+    ]
+  },
+    {
+    numb: 3,
+    question: "How many colors are in a rainbow?",
+    answer: "Seven",
+    options: [
+      "Seven",
+      "Ten",
+      "5",
+      "8"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What's the name of the place where you go see a lot of animals?",
+    answer: "The Zoo",
+    options: [
+      "Walmart",
+      "The Zoo",
+      "The Mall",
+      "Your backyard"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does HTML stand for?",
+    answer: "Hypertext Markup Language",
+    options: [
+      "Help Me Miss Lois",
+      "Hypertext Markup Language",
+      "Help Tom Meet Ladies",
+      "Hyper Tool Multi Language"
+    ]
+  },
+
+];
 
